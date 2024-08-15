4c8d3a3a78bba582054d9dff96d1d44a962690e4
Md Suny Shaikh <mdsunyshaikh@gmail.com>
2024-08-16 00:37:33 +0600

    include axios interceptor

--- src/Hooks/useAxiosSecure.jsx
+++ src/Hooks/useAxiosSecure.jsx
@@ -1,11 +1,31 @@
 import axios from "axios"
+import useAuth from "./useAuth"
+import { useNavigate } from "react-router-dom"
+import { useEffect } from "react"
 
 export const axiosSecure = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
-  // withCredentials: true,
+  withCredentials: true,
 })
 
 const useAxiosSecure = () => {
+  const { userSignOut } = useAuth()
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    axiosSecure.interceptors.response.use(
+      (res) => res,
+      async (error) => {
+        console.log("Axios interceptors error", error.response)
+        if (error.response.status === 401 || error.response.status === 403) {
+          await userSignOut()
+          navigate("/login")
+        }
+        return Promise.reject(error)
+      }
+    )
+  }, [navigate, userSignOut])
+
   return axiosSecure
 }
 
